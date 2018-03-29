1ab543460d46b48926257208693d47d0b540d366
Kelsey Fecho <kfecho30@gmail.com>
2018-03-29 17:29:45 +0000

    automatically backed up by learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -3,10 +3,10 @@ class Pokemon
   def initialize(name = nil, type = nil, db = nil)
     @name = name,
     @type = type,
-    @database_connection = SQLite3::Database.new(db)
+    database_connection = SQLite3::Database.new(db)
   end
 
   def self.save(id, name, type)
-    @database_connection.execute("INSERT INTO pokemon(id, name, type) VALUES (?, ?, ?)", id, name, type)
+    database_connection.execute("INSERT INTO pokemon(id, name, type) VALUES (?, ?, ?)", id, name, type)
   end
 end
